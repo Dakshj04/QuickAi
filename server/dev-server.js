0ccb40f21072a7cf182872b2cedfff9997b4f744
Dakshj04 <0408jaindaksh@gmail.com>
2025-07-29 18:41:15 +0530

    final deployment try

--- server/dev-server.js
+++ server/dev-server.js
@@ -1,5 +1,6 @@
-import app from './server.js';
+import app from './api/server.js';
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+  console.log(`Local Dev Server running on port ${PORT}`);
 });
